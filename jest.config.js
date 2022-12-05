f5b6b69a5a121a946d32e61cb88087a208e3b545
Carmen Alvarez <carmen@rmen.ca>
2022-12-05 21:36:07 +0100

    Add github actions workflow to run tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 const config = {
   modulePaths: ['<rootDir>/src/'],
+  reporters: [ 
+    "default", 
+    ["jest-junit", {
+      outputDirectory: 'reports',
+      outputName: 'junit.xml',
+    }]
+  ],
 };
 
 module.exports = config;
